cba9ffba547ec569c43a6a29b9c5985696761e3f
satoeri0502 <sato.eriko0502@gmail.com>
2025-07-06 19:23:03 +0900

    Fix: デプロイ用に設定ファイル修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,19 @@
 require "sidekiq/cron/job"
+# RedisのURLを環境変数から取得（なければローカル用）
+redis_url = ENV.fetch('REDIS_URL') { 'redis://localhost:6379/0' }
 
-Sidekiq::Cron::Job.load_from_hash!(
-  reminder_schedule_job: {
-    cron: "0 3 * * *", # 毎朝3時に実行（日本時間）
-    class: "ReminderScheduleJob"
-  }
-)
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+
+  # Cronジョブはサーバー側でのみ設定
+  Sidekiq::Cron::Job.load_from_hash!(
+    reminder_schedule_job: {
+      cron: "0 18 * * *", # 毎朝3時に実行（日本時間）Renderは UTCで動いているので-9時間
+      class: "ReminderScheduleJob"
+    }
+  )
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+end
