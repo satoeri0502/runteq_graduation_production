8dc2825fa365d546dafd456986baa26703c46600
satoeri0502 <sato.eriko0502@gmail.com>
2025-07-06 22:57:05 +0900

    Fix: LINE設定ファイル修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -222,8 +222,8 @@ Rails.application.config.sorcery.configure do |config|
   # config.line.key = Rails.application.credentials.dig(:line, :channel_id)
   # config.line.secret = Rails.application.credentials.dig(:line, :login_channel_secret)
   # config.line.callback_url = Rails.application.config.line_callback_url
-  config.line.client_id = ENV["LINE_CHANNEL_ID"]
-  config.line.client_secret = ENV["LINE_CHANNEL_SECRET"]
+  config.line.key = ENV["LINE_CHANNEL_ID"]
+  config.line.secret = ENV["LINE_CHANNEL_SECRET"]
   config.line.callback_url = ENV["LINE_CALLBACK_URL"]
   config.line.scope = "profile"
   # config.line.bot_prompt = "normal"
